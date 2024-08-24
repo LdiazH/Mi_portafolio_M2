72b40778019a75dce10c0b280a3ec134b80712f0
Luis <luis.e.diaz.h@gmail.com>
2024-08-23 21:18:36 -0400

    Avance_3

---
+++ Assets/script/script.js
@@ -0,0 +1,13 @@
+document.getElementById("contactForm").addEventListener("submit",function(event){
+    event.preventDefault();
+})
+
+
+document.getElementById("submitBtn").addEventListener("click",function(){
+    alert("Muchas gracias, recibirá una mensaje de vuelta a la brevedad.")
+})
+
+document.getElementsByClassName(".nav-link").addEventListener("mouseover",function(){
+    
+
+})
